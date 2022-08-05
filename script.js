50f983744b25c89f1c687ca2a55aa0dda9f630ef
Samuel Ercanbrack <sam.ercanbrack@outlook.com>
2022-08-04 19:23:56 -0600

    added class project 2 to portfolio

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var featuredProjectEl = document.getElementById('featured-project')
+var otherProjectOne = document.getElementById('other-project-one')
 
 featuredProjectEl.onclick = function() {
     window.open('https://github.com/ciaragauntt/MedTracker')
+}
+
+otherProjectOne.onclick = function() {
+    window.open('https://github.com/Sercanbrack/dnd-right-here')
 }
